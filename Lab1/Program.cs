a5f3eb7d469d25cd608ccc0b88bddbbad0f71a25
nikit <nikit@DESKTOP-769CKJJ>
2023-11-12 12:50:28 +0500

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace LabOne
+{
+    internal class Program
+    {
+
+        public static void Main()
+        {
+            Console.Write("Введите математическое выражение: ");
+            string mathExpression = Console.ReadLine().Replace(" ", "");
+
+            List<int> numbers = new List<int>();
+            List<char> operators = new List<char>();
+
+            int i = 0;
+            string number = "";
+
+            while (i < mathExpression.Length)
+            {
+                if (char.IsDigit(mathExpression[i]))
+                {
+                    number += Convert.ToString(mathExpression[i]);
+
+                    if (i == mathExpression.Length - 1)
+                        numbers.Add(int.Parse(number));
+
+                    i++;
+                }
+
+                else
+                {
+                    numbers.Add(int.Parse(number));
+                    number = "";
+
+                    operators.Add(mathExpression[i]);
+                    i++;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+            Console.WriteLine(string.Join(" ", operators));
+        }
+    }
+}
